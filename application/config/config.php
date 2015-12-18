10f5762607c457bf81468b8ab1f9d06b9e340b6f
Gian Carlo Tomakin <gctomakin@fixerinit.co.jp>
2015-12-19 07:41:14 +0800

    Added configurations

--- application/config/config.php
+++ application/config/config.php
@@ -314,7 +314,7 @@ $config['cache_query_string'] = FALSE;
 | http://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'UUvcf9XSPHm7AR40sd7Mzg====';
 
 /*
 |--------------------------------------------------------------------------
