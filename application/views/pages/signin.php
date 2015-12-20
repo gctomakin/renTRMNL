a1ba9c2248281376a81da2da853d59915ade827a
Gian Carlo Tomakin <gctomakin@fixerinit.co.jp>
2015-12-20 22:49:28 +0800

    Modified sign-in form

--- application/views/pages/signin.php
+++ application/views/pages/signin.php
@@ -2,14 +2,14 @@
 <div class="container">
     <div class="row">
         <div class="col-sm-6 col-md-4 col-md-offset-4">
-            <h1 class="text-center login-title">Sign in to continue to Bootsnipp</h1>
-            <?php if($this->session->flashdata('error')) { echo '<p class="alert alert-danger"><strong>Login Failed!</strong> Invalid username/password :(</p>'; }?>
+            <h1 class="text-center login-title">Sign in to continue to renTRMNL</h1>
+            <?php if($this->session->flashdata('error')) { echo '<p class="alert alert-danger text-center"><strong>Login Failed!</strong> Invalid username/password :(</p>'; }?>
             <div class="account-wall">
                 <img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
                     alt="">
-                <form class="form-signin">
-                <input type="text" class="form-control" placeholder="Email" required autofocus>
-                <input type="password" class="form-control" placeholder="Password" required>
+                <?php $attributes = array('class' => 'form-signin', 'id' => 'form-signin'); echo form_open('signin', $attributes); ?>
+                <input type="text" name="username" class="form-control" placeholder="Email" required autofocus>
+                <input type="password" name="password" class="form-control" placeholder="Password" required>
                 <button class="btn btn-lg btn-primary btn-block" type="submit">
                     Sign in</button>
                 <label class="checkbox pull-left">
@@ -17,7 +17,7 @@
                     Remember me
                 </label>
                 <a href="#" class="pull-right need-help">Need help? </a><span class="clearfix"></span>
-                </form>
+                <?php echo form_close(); ?>
             </div>
             <a href="<?php echo site_url("/"); ?>" class="text-center new-account">Go back to home page </a>
         </div>
