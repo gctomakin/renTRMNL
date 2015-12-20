955486c3f791325892e466ecf25cfd220a3f47e6
gian <gctomakin@fixerinit.co.jp>
2015-12-20 09:49:03 +0800

    Modified

--- application/views/pages/admin.php
+++ application/views/pages/admin.php
@@ -1,3 +1,13 @@
+<header>
+    <div class="header-content">
+        <div class="header-content-inner">
+            <h1>A Software-as-a-Service for Rental Shops</h1>
+            <hr>
+            <p>renTRMNL can help you build better websites using the Bootstrap CSS framework! Just download your template and start going, no strings attached!</p>
+            <a href="#about" class="btn btn-primary btn-xl page-scroll">Find Out More</a>
+        </div>
+    </div>
+</header>
 <section id="admin">
     <div class="container">
         <div class="row">
