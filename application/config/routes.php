29dfb12b1ca9650d73000af69a6a6e0f2d59aa04
Gian Carlo Tomakin <gctomakin@fixerinit.co.jp>
2015-12-20 18:46:46 +0800

    Added routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,5 +51,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'main';
 $route['admin'] = 'main/admin';
+$route['lessee/signup'] = 'main/signUp';
+$route['lessee/login'] = 'main/lesseeLogin';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
