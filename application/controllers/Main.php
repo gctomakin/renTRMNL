5f520031081dca581869bd23b7d747ba9b20c018
Gian Carlo Tomakin <gctomakin@fixerinit.co.jp>
2015-12-20 18:46:19 +0800

    Added lessee signup and login methods

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -3,14 +3,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
 
+  public function __construct()
+  {
+      parent::__construct();
+      $this->load->model('Lessees');
+  }
+
 	public function index()
   {
     // $data['title'] = 'New Title';
-    
-    $data['content'] = $this->load->view('pages/main', '', true);
-    
+
+    $data['content'] = $this->load->view('pages/main', '', TRUE);
+
     /* SAMPLE of Additional Styles and Scripts */
-    
+
     // $data['style'] = array(
     //   'folder/thestyle',
     //   'folder/thestyle2'
@@ -19,14 +25,83 @@ class Main extends CI_Controller {
     //   'folder/script1',
     //   'folder/script2'
     // );
-    
+
     $this->load->view('common/main', $data);
 	}
 
   public function admin()
   {
-    $data['content'] = $this->load->view('pages/admin', '', true);
+    $data['content'] = $this->load->view('pages/admin', '', TRUE);
     $this->load->view('common/main', $data);
+  }
+
+  public function signUp()
+  {
+    // field name, error message, validation rules
+    $this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[4]|xss_clean');
+    $this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[4]|max_length[32]|xss_clean');
+    $this->form_validation->set_rules('password2', 'Password Confirmation', 'trim|required|matches[password]|xss_clean');
+    $this->form_validation->set_rules('lessee_fname', 'Firstname', 'trim|required|xss_clean');
+    $this->form_validation->set_rules('lessee_lname', 'Lastname', 'trim|required|xss_clean');
+    $this->form_validation->set_rules('lessee_email', 'Email', 'trim|required|valid_email|xss_clean');
+    $this->form_validation->set_rules('lessee_phoneno', 'Phone No', 'trim|required|xss_clean');
+
+    if($this->form_validation->run() == FALSE):
+
+      $this->index();
+
+    else:
+
+      $data = array('username'=>$this->input->post('username'),
+                    'password'=>$this->encrypt->encode($this->input->post('password')),
+                    'lessee_fname'=>$this->input->post('lessee_fname'),
+                    'lessee_lname'=>$this->input->post('lessee_lname'),
+                    'lessee_email'=>$this->input->post('lessee_email'),
+                    'lessee_phoneno'=>$this->input->post('lessee_phoneno'));
+
+      $result = $this->Lessees->create($data);
+
+      echo $result;
+
+    endif;
+
+
+  }
+
+  public function lesseeLogin()
+  {
+    $data['error']=0;
+
+    if($_POST){
+
+      $username = $this->input->post('username',TRUE);
+      $password = $this->input->post('password',TRUE);
+      $user = $this->Lessees->login($username,$password);
+
+      if(!empty( $user ) && $this->encrypt->decode($user['password']) == $password){
+
+        $newdata = array('lessee_id' => $user['lessee_id'],
+                         'username' => $user['username'],
+                         'lessee_fname' => $user['lessee_fname'],
+                         'lessee_lname' => $user['lessee_lname'],
+                         'lessee_email' => $user['lessee_email'],
+                         'lessee_phoneno' => $user['lessee_phoneno'],
+                         'logged_in' => TRUE);
+        $this->session->set_userdata($newdata);
+        redirect(base_url().'main/lesseeDashboard');
+
+      } else {
+
+        $data['error']=1;
+        $this->load->view('common/main', $data);
+
+      }
+    }
+  }
+
+  public function lesseeDashboard()
+  {
+    echo 'TODO';
   }
 
 
