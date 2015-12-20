5ad56f842cc31aa1e970cd27d8b6f4f700e9421a
Gian Carlo Tomakin <gctomakin@fixerinit.co.jp>
2015-12-20 10:47:39 +0800

    Modified

--- application/views/templates/mainFooter.php
+++ application/views/templates/mainFooter.php
@@ -1,16 +1,16 @@
     <!-- jQuery -->
-    <script src="<?php echo base_url("bower_components/jquery/dist/jquery.min.js"); ?>"></script>
+    <script src="<?php echo site_url("bower_components/jquery/dist/jquery.min.js"); ?>"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="<?php echo base_url("bower_components/bootstrap/dist/js/bootstrap.min.js"); ?>"></script>
+    <script src="<?php echo site_url("bower_components/bootstrap/dist/js/bootstrap.min.js"); ?>"></script>
 
     <!-- Plugin JavaScript -->
-    <script src="<?php echo base_url("js/jquery.easing.min.js"); ?>"></script>
-    <script src="<?php echo base_url("js/jquery.fittext.js"); ?>"></script>
-    <script src="<?php echo base_url("js/wow.min.js"); ?>"></script>
+    <script src="<?php echo site_url("assets/js/jquery.easing.min.js"); ?>"></script>
+    <script src="<?php echo site_url("assets/js/jquery.fittext.js"); ?>"></script>
+    <script src="<?php echo site_url("assets/js/wow.min.js"); ?>"></script>
 
     <!-- Custom Theme JavaScript -->
-    <script src="<?php echo base_url("js/creative.js"); ?>"></script>
+    <script src="<?php echo site_url("assets/js/creative.js"); ?>"></script>
 
 </body>
 
