097ba408876f2d0a3690d0ddaaba93149ff5441d
Gian Carlo Tomakin <gctomakin@fixerinit.co.jp>
2015-12-20 09:14:38 +0800

    Added admin page

---
+++ application/views/pages/admin.php
@@ -0,0 +1,10 @@
+<section id="admin">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <h2 class="section-heading">Admin</h2>
+                <hr class="primary">
+            </div>
+        </div>
+    </div>
+</section>
