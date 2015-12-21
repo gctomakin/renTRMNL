ac0ebc177b6f26f9ed516d9197157e90d08ef8d2
Gian Carlo Tomakin <gctomakin@fixerinit.co.jp>
2015-12-22 06:49:15 +0800

    Added warning msg for invalid user type

--- application/views/pages/signin.php
+++ application/views/pages/signin.php
@@ -3,7 +3,14 @@
     <div class="row">
         <div class="col-sm-6 col-md-4 col-md-offset-4">
             <h1 class="text-center login-title">Sign in to continue to renTRMNL</h1>
-            <?php if($this->session->flashdata('error')) { echo '<p class="alert alert-danger text-center"><strong>Login Failed!</strong> Invalid username/password :(</p>'; }?>
+            <?php
+            if($this->session->flashdata('error')) {
+                echo '<p class="alert alert-danger text-center"><strong>Login Failed!</strong> Invalid username/password :(</p>';
+             }
+            if($this->session->flashdata('warning')) {
+                echo '<p class="alert alert-warning text-center"><strong>Login Failed!</strong> Invalid Usertype :(</p>';
+            }
+            ?>
             <div class="account-wall">
                 <!--<img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120" alt=""> -->
                 <?php
@@ -22,7 +29,7 @@
                         </label>
                     </div>
                 </div>
-                <input type="text" name="username" class="form-control" placeholder="Email" required autofocus>
+                <input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
                 <input type="password" name="password" class="form-control" placeholder="Password" required>
                 <button class="btn btn-lg btn-primary btn-block" type="submit">
                     Sign in</button>
@@ -37,3 +44,7 @@
         </div>
     </div>
 </div>
+<script type="text/javascript">
+    var signinLesseeUrl = "<?php echo site_url('signin'); ?>";
+    var signinLessorUrl = "<?php echo site_url('lessors/signin'); ?>";
+</script>
