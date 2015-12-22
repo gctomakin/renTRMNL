844df34b2b829ab4ae52af1cb3545ac85baf221e
Nichole Martinez <xin_martinez@yahoo.com>
2015-12-23 00:07:50 +0800

    Add common page for lessee with templates

---
+++ application/views/common/lessee.php
@@ -0,0 +1,26 @@
+<?php
+
+$data['content'] = $this->load->view('templates/lessee/nav', '', true);
+$data['content'] .= $this->load->view('templates/lessee/sidebar', '', true);
+
+$wrapper = array(
+	'content' => $content,
+	'title' => $title
+); 
+$data['content'] .= $this->load->view('templates/lessee/wrapper', $wrapper, true);
+
+$data['script'] 	= empty($script) ? array() : $script;
+$data['script'][] = 'metisMenu.min';
+$data['script'][] = 'sb-admin-2';
+
+$data['style'] 		= empty($style) ? array() : $style;
+$data['style'][] 	= 'new-bootstrap.min';
+$data['style'][] 	= 'metisMenu.min';
+$data['style'][] 	= 'sb-admin-2';
+
+$data['hasNewBootrstap'] = true;
+
+
+
+
+$this->load->view('base', $data);
