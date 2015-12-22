cc5158af309c8d6aee76e827db8cdae622186a54
Gian Carlo Tomakin <gctomakin@fixerinit.co.jp>
2015-12-22 22:53:59 +0800

    Added getters and setters

--- application/models/Lessee.php
+++ application/models/Lessee.php
@@ -2,23 +2,154 @@
 
 class Lessee extends CI_Model{
 
-  public function create($data)
+  private $id;
+  private $fname;
+  private $lname;
+  private $email;
+  private $phoneno;
+  private $username;
+  private $password;
+
+  private function getId()
   {
-
-    $this->db->insert('lessees',$data);
-
-    return TRUE;
-
+    return $this->id;
   }
 
-  public function login($username,$password)
+  public function setId($id)
   {
+    $this->id = $id;
+  }
 
-    $this->db->select()->from('lessees')->where('username', $username);
-    $query=$this->db->get();
+  private function getFname()
+  {
+    return $this->fname;
+  }
+
+  public function setFname($fname)
+  {
+    $this->fname = $fname;
+  }
+
+  private function getLname()
+  {
+    return $this->lname;
+  }
+
+  public function setLname($lname){
+    $this->lname = $lname;
+  }
+
+  private function getEmail()
+  {
+    return $this->email;
+  }
+
+  public function setEmail($email)
+  {
+    $this->email = $email;
+  }
+
+  private function getPhoneno()
+  {
+    return $this->phoneno;
+  }
+
+  public function setPhoneno($phoneno)
+  {
+    $this->phoneno = $phoneno;
+  }
+
+  private function getUsername()
+  {
+    return $this->username;
+  }
+
+  public function setUsername($username)
+  {
+    $this->username = $username;
+  }
+
+  private function getPassword()
+  {
+    return $this->password;
+  }
+
+  public function setPassword($password)
+  {
+    $this->password = $password;
+  }
+
+  public function authenticate($username, $password)
+  {
+    $user = $this->findByUsername($username);
+
+    if(!empty( $user ) && $this->encrypt->decode($user['password']) == $password):
+
+      return $user;
+
+    else:
+
+      return FALSE;
+
+    endif;
+  }
+
+  public function all()
+  {
+    $query = $this->db->get('lessees');
+
+    return $query;
+  }
+
+  public function insert()
+  {
+    $data['lessee_fname'] = $this->getFname();
+    $data['lessee_lname'] = $this->getLname();
+    $data['lessee_email'] = $this->getEmail();
+    $data['lessee_phoneno'] = $this->getPhoneno();
+    $data['username'] = $this->getUsername();
+    $data['password'] = $this->getPassword();
+    $this->db->set($data);
+    $this->db->insert('lessees');
+
+    return TRUE;
+  }
+
+  public function update($id, $data)
+  {
+    $this->db->where('lessee_id', $id);
+    $this->db->update('lessees', $data);
+
+    return TRUE;
+  }
+
+  public function delete($id)
+  {
+    $this->db->where('lessee_id', $id);
+    $this->db->delete('lessees');
+
+    return TRUE;
+  }
+
+  public function findByUsername($username)
+  {
+    $query = $this->db->get_where('lessees', array('username' => $username));
 
     return $query->first_row('array');
+  }
 
+  public function findById($id)
+  {
+    $query = $this->db->get_where('lessees', array('lessee_id' => $id));
+
+    return $query->first_row('array');
+  }
+
+  public function findByEmail($email)
+  {
+    $query = $this->db->get_where('lessees', array('lessee_email' => $email));
+
+    return $query->first_row('array');
   }
 
 }
