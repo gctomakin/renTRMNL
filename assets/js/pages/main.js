c43923ce9240453439071c8e4a4d08b0484d1529
Gian Carlo Tomakin <gctomakin@fixerinit.co.jp>
2015-12-25 08:59:03 +0800

    Added script

--- assets/js/pages/main.js
+++ assets/js/pages/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function() {
   $('.btn-signin-type').click(function() {
   	var action = ($(this).find('input').val() == 'lessee') ? signinLesseeUrl : signinLessorUrl;
+    if($(this).find('input').val() == 'lessee'){
+      $('#social-media-login').show();
+    } else {
+      $('#social-media-login').hide();
+    }
     $('#form-signin').attr('action', action);
   });
-}); 
+});
