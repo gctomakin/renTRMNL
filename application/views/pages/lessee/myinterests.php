932a64a7e963f01138faed21427b06ac3e5c26ae
Gian Carlo Tomakin <gctomakin@fixerinit.co.jp>
2015-12-26 22:58:50 +0800

    Added myineterst and myshops views

---
+++ application/views/pages/lessee/myinterests.php
@@ -0,0 +1,95 @@
+<div class="container-fluid">
+  <div class="row no-gutter">
+    <div class="col-sm-4 col-lg-4 col-md-4">
+        <div class="thumbnail">
+            <img src="http://placehold.it/320x150" alt="">
+            <div class="caption">
+                <div class="btn-group inline pull-right" data-toggle="buttons-checkbox">
+                  <a href="#" class="btn btn-info btn-sm">More Info</a>
+                  <a href="#" class="btn btn-primary btn-sm">My Shop</a>
+                </div>
+                <h4><a href="#">First Interest</a>
+                </h4>
+                <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-4 col-lg-4 col-md-4">
+        <div class="thumbnail">
+            <img src="http://placehold.it/320x150" alt="">
+            <div class="caption">
+                <div class="btn-group inline pull-right" data-toggle="buttons-checkbox">
+                  <a href="#" class="btn btn-info btn-sm">More Info</a>
+                  <a href="#" class="btn btn-primary btn-sm">My Shop</a>
+                </div>
+                <h4><a href="#">Second Interest</a>
+                </h4>
+                <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-4 col-lg-4 col-md-4">
+        <div class="thumbnail">
+            <img src="http://placehold.it/320x150" alt="">
+            <div class="caption">
+                <div class="btn-group inline pull-right" data-toggle="buttons-checkbox">
+                  <a href="#" class="btn btn-info btn-sm">More Info</a>
+                  <a href="#" class="btn btn-primary btn-sm">My Shop</a>
+                </div>
+                <h4><a href="#">Third Interest</a>
+                </h4>
+                <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="col-sm-4 col-lg-4 col-md-4">
+        <div class="thumbnail">
+            <img src="http://placehold.it/320x150" alt="">
+            <div class="caption">
+                <div class="btn-group inline pull-right" data-toggle="buttons-checkbox">
+                  <a href="#" class="btn btn-info btn-sm">More Info</a>
+                  <a href="#" class="btn btn-primary btn-sm">My Shop</a>
+                </div>
+                <h4><a href="#">Fourth Interest</a>
+                </h4>
+                <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-4 col-lg-4 col-md-4">
+        <div class="thumbnail">
+            <img src="http://placehold.it/320x150" alt="">
+            <div class="caption">
+                <div class="btn-group inline pull-right" data-toggle="buttons-checkbox">
+                  <a href="#" class="btn btn-info btn-sm">More Info</a>
+                  <a href="#" class="btn btn-primary btn-sm">My Shop</a>
+                </div>
+                <h4><a href="#">Fifth Interest</a>
+                </h4>
+                <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-4 col-lg-4 col-md-4">
+        <div class="thumbnail">
+            <img src="http://placehold.it/320x150" alt="">
+            <div class="caption">
+                <div class="btn-group inline pull-right" data-toggle="buttons-checkbox">
+                  <a href="#" class="btn btn-info btn-sm">More Info</a>
+                  <a href="#" class="btn btn-primary btn-sm">My Shop</a>
+                </div>
+                <h4><a href="#">Fifth Interest</a>
+                </h4>
+                <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+        </div>
+    </div>
+
+</div>
+</div>
---
+++ application/views/pages/lessee/myshops.php
@@ -0,0 +1,99 @@
+<div class="container-fluid">
+  <div class="row no-gutter">
+      <div class="col-md-7">
+          <a href="#">
+              <img class="img-responsive" src="http://placehold.it/700x300" alt="">
+          </a>
+      </div>
+      <div class="col-md-5">
+          <h3>My Shop One</h3>
+          <h4>Subheading</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
+          <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+      </div>
+  </div>
+  <hr>
+  <div class="row no-gutter">
+      <div class="col-md-7">
+          <a href="#">
+              <img class="img-responsive" src="http://placehold.it/700x300" alt="">
+          </a>
+      </div>
+      <div class="col-md-5">
+          <h3>My Shop One</h3>
+          <h4>Subheading</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
+          <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+      </div>
+  </div>
+  <hr>
+  <div class="row no-gutter">
+      <div class="col-md-7">
+          <a href="#">
+              <img class="img-responsive" src="http://placehold.it/700x300" alt="">
+          </a>
+      </div>
+      <div class="col-md-5">
+          <h3>My Shop One</h3>
+          <h4>Subheading</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
+          <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+      </div>
+  </div>
+  <hr>
+  <div class="row no-gutter">
+      <div class="col-md-7">
+          <a href="#">
+              <img class="img-responsive" src="http://placehold.it/700x300" alt="">
+          </a>
+      </div>
+      <div class="col-md-5">
+          <h3>My Shop One</h3>
+          <h4>Subheading</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
+          <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+      </div>
+  </div>
+  <hr>
+  <div class="row no-gutter">
+      <div class="col-md-7">
+          <a href="#">
+              <img class="img-responsive" src="http://placehold.it/700x300" alt="">
+          </a>
+      </div>
+      <div class="col-md-5">
+          <h3>My Shop One</h3>
+          <h4>Subheading</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
+          <a class="btn btn-primary" href="#">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>
+      </div>
+  </div>
+    <!--pagination-->
+    <div class="row text-center">
+      <div class="col-lg-12">
+          <ul class="pagination">
+              <li>
+                  <a href="#">«</a>
+              </li>
+              <li class="active">
+                  <a href="#">1</a>
+              </li>
+              <li>
+                  <a href="#">2</a>
+              </li>
+              <li>
+                  <a href="#">3</a>
+              </li>
+              <li>
+                  <a href="#">4</a>
+              </li>
+              <li>
+                  <a href="#">5</a>
+              </li>
+              <li>
+                  <a href="#">»</a>
+              </li>
+          </ul>
+      </div>
+  </div>
+</div>
