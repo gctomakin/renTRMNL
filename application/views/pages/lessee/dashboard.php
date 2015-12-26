769547491e05d6b3722af5abfeb458a38afb0336
Gian Carlo Tomakin <gctomakin@fixerinit.co.jp>
2015-12-26 23:00:12 +0800

    Added html

--- application/views/pages/lessee/dashboard.php
+++ application/views/pages/lessee/dashboard.php
@@ -1,3 +1,86 @@
-<?php
-echo $this->session->userdata('lessee_id');
-?>
+<div class="container-fluid">
+        <div class="row no-gutter">
+            <div class="col-lg-4 col-sm-6 portfolio-item">
+                <a href="#" class="portfolio-box">
+                  <img src="http://localhost/rentrmnl/assets/img/portfolio/1.jpg" class="img-responsive" alt="">
+                  <h3>
+                    <a href="#">Category Name</a>
+                  </h3>
+                </a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 portfolio-item">
+                <a href="#" class="portfolio-box">
+                  <img src="http://localhost/rentrmnl/assets/img/portfolio/2.jpg" class="img-responsive" alt="">
+                  <h3>
+                    <a href="#">Category Name</a>
+                  </h3>
+                </a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 portfolio-item">
+                <a href="#" class="portfolio-box">
+                  <img src="http://localhost/rentrmnl/assets/img/portfolio/3.jpg" class="img-responsive" alt="">
+                  <h3>
+                    <a href="#">Category Name</a>
+                  </h3>
+                </a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 portfolio-item">
+                <a href="#" class="portfolio-box">
+                  <img src="http://localhost/rentrmnl/assets/img/portfolio/4.jpg" class="img-responsive" alt="">
+                  <h3>
+                    <a href="#">Category Name</a>
+                  </h3>
+                </a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 portfolio-item">
+                <a href="#" class="portfolio-box">
+                  <img src="http://localhost/rentrmnl/assets/img/portfolio/5.jpg" class="img-responsive" alt="">
+                  <h3>
+                    <a href="#">Category Name</a>
+                  </h3>
+                </a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+            </div>
+            <div class="col-lg-4 col-sm-6 portfolio-item">
+                <a href="#" class="portfolio-box">
+                  <img src="http://localhost/rentrmnl/assets/img/portfolio/6.jpg" class="img-responsive" alt="">
+                  <h3>
+                    <a href="#">Category Name</a>
+                  </h3>
+                </a>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+            </div>
+        </div>
+        <!--pagination-->
+        <div class="row text-center">
+          <div class="col-lg-12">
+              <ul class="pagination">
+                  <li>
+                      <a href="#">«</a>
+                  </li>
+                  <li class="active">
+                      <a href="#">1</a>
+                  </li>
+                  <li>
+                      <a href="#">2</a>
+                  </li>
+                  <li>
+                      <a href="#">3</a>
+                  </li>
+                  <li>
+                      <a href="#">4</a>
+                  </li>
+                  <li>
+                      <a href="#">5</a>
+                  </li>
+                  <li>
+                      <a href="#">»</a>
+                  </li>
+              </ul>
+          </div>
+      </div>
+    </div>
