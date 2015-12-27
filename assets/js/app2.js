aa43f3a1e5db8c1caaf53fac1ed3a4f1a4e829da
Gian Carlo Tomakin <gctomakin@fixerinit.co.jp>
2015-12-28 00:40:29 +0800

    Added pusher script

---
+++ assets/js/app2.js
@@ -0,0 +1,17 @@
+// Enable pusher logging - don't include this in production
+// Pusher.log = function(message) {
+//   if (window.console && window.console.log) {
+//     window.console.log(message);
+//   }
+// };
+
+var pusher = new Pusher('b3c7fc474d668cd4563e', {
+  encrypted: true
+});
+var channel = pusher.subscribe('msg_channel');
+channel.bind('onMessage', function(data) {
+  console.log(data);
+  var template = _.template($("#msg-template").html());
+  var tmpl = template({subject: data.subject, message: data.message, date: data.date});
+  $("#table-message tbody").append(tmpl);
+});
