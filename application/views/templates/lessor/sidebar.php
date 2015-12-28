e0c5bba44943da9e759b4ed530462c484219e45a
Nichole Martinez <xin_martinez@yahoo.com>
2015-12-29 01:50:13 +0800

    Fix lessor's sidebar
    Shops and Items menu

--- application/views/templates/lessor/sidebar.php
+++ application/views/templates/lessor/sidebar.php
@@ -13,22 +13,22 @@
                     <!-- /input-group -->
                 </li>
                 <li>
-                    <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
+                    <a href="<?php echo site_url('lessor/dashboard'); ?>"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
                 </li>
                 <li>
-                    <a href="#"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
+                    <a href="#shops"><i class="fa fa-table fa-fw"></i> Shops<span class="fa arrow"></span></a>
                     <ul class="nav nav-second-level">
-                        <li>
-                            <a href="flot.html">Flot Charts</a>
-                        </li>
-                        <li>
-                            <a href="morris.html">Morris.js Charts</a>
-                        </li>
+                        <li><a href="<?php echo site_url('lessor/shops/create'); ?>">Create</a></li>
+                        <li><a href="<?php echo site_url('lessor/shops/list'); ?>">List</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#item-nav"><i class="fa fa-cubes fa-fw"></i> Items<span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level">
+                        <li><a href="<?php echo site_url('lessor/items/create'); ?>">Create</a></li>
+                        <li><a href="<?php echo site_url('lessor/items'); ?>">List</a></li>
                     </ul>
                     <!-- /.nav-second-level -->
-                </li>
-                <li>
-                    <a href="tables.html"><i class="fa fa-table fa-fw"></i> Tables</a>
                 </li>
                 <li>
                     <a href="forms.html"><i class="fa fa-edit fa-fw"></i> Forms</a>
