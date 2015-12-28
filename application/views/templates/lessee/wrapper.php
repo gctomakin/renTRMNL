90d0e6a06625374e31ef1dbf4b4075016d8caab3
Gian Carlo Tomakin <gctomakin@fixerinit.co.jp>
2015-12-28 10:37:20 +0800

    Added page load time

--- application/views/templates/lessee/wrapper.php
+++ application/views/templates/lessee/wrapper.php
@@ -2,6 +2,7 @@
 	<div class="row">
       <div class="col-lg-12">
           <h1 class="page-header"><?php echo empty($title) ? 'No-Title' : $title; ?></h1>
+          Page rendered in <strong>{elapsed_time}</strong> seconds.
       </div>
       <!-- /.col-lg-12 -->
   </div>
