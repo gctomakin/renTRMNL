63df21b5b2932f5911f1a4602e249534ab7461c3
Gian Carlo Tomakin <gctomakin@fixerinit.co.jp>
2015-12-29 14:37:18 +0800

    Added admin tenmplates

---
+++ application/views/templates/admin/wrapper.php
@@ -0,0 +1,11 @@
+<div id="page-wrapper">
+	<div class="row">
+      <div class="col-lg-12">
+          <h1 class="page-header"><?php echo empty($title) ? 'No-Title' : $title; ?></h1>
+      </div>
+      <!-- /.col-lg-12 -->
+  </div>
+  <div class="row">
+  	<?php echo $content; ?>
+  </div>
+</div>
