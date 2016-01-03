a99ff9ec89aa2729faa9985cc7eba78b84f9f085
Gian Carlo Tomakin <gctomakin@fixerinit.co.jp>
2016-01-03 11:38:59 +0800

    Modified

--- application/models/Lessee.php
+++ application/models/Lessee.php
@@ -3,246 +3,297 @@
 class Lessee extends CI_Model
 {
 
-    private $id;
-    private $fname;
-    private $lname;
-    private $email;
-    private $phoneno;
-    private $username;
-    private $password;
-    private $image;
-    private $lessees_table = 'lessees';
-    private $myinterests_table = 'my_interests';
-    private $limit = 20;
-    private $offset = 0;
+  private $id;
+  private $fname;
+  private $lname;
+  private $email;
+  private $phoneno;
+  private $username;
+  private $password;
+  private $image;
+  private $lessees_table = 'lessees';
+  private $myinterests_table = 'my_interests';
+  private $limit = 20;
+  private $offset = 0;
 
-    private function getId()
-    {
-        return $this->id;
+  private function getId()
+  {
+    return $this->id;
+  }
+
+  public function setId($id)
+  {
+    $this->id = $id;
+  }
+
+  private function getFname()
+  {
+    return $this->fname;
+  }
+
+  public function setFname($fname)
+  {
+    $this->fname = $fname;
+  }
+
+  private function getLname()
+  {
+    return $this->lname;
+  }
+
+  public function setLname($lname)
+  {
+    $this->lname = $lname;
+  }
+
+  private function getEmail()
+  {
+    return $this->email;
+  }
+
+  public function setEmail($email)
+  {
+    $this->email = $email;
+  }
+
+  private function getPhoneno()
+  {
+    return $this->phoneno;
+  }
+
+  public function setPhoneno($phoneno)
+  {
+    $this->phoneno = $phoneno;
+  }
+
+  private function getUsername()
+  {
+    return $this->username;
+  }
+
+  public function setUsername($username)
+  {
+    $this->username = $username;
+  }
+
+  private function getPassword()
+  {
+    return $this->password;
+  }
+
+  public function setPassword($password)
+  {
+    $this->password = $password;
+  }
+
+  private function getImage()
+  {
+    return $this->image;
+  }
+
+  public function setImage($image)
+  {
+    $this->image = $image;
+  }
+
+  public function authenticate()
+  {
+    $user = $this->findByUsername($this->getUsername());
+
+    if (!empty($user) && $this->encrypt->decode($user['password']) == $this->getPassword()):
+      return $user;
+    else:
+      return false;
+    endif;
+  }
+
+  public function googleLogin($user)
+  {
+    $this->setUsername($user['id']);
+    $result = $this->findByUsername($this->getUsername());
+
+    if (!empty($result)):
+      $this->db->cache_delete('default', 'index');
+      return $result;
+    else:
+      $data['lessee_fname']   = $user['givenName'];
+      $data['lessee_lname']   = $user['familyName'];
+      $data['lessee_email']   = $user['email'];
+      $data['lessee_phoneno'] = "";
+      $data['username']       = $user['id'];
+      $data['password']       = "";
+      $this->db->set($data);
+      $this->db->insert($this->lessees_table);
+      $lastId = $this->db->insert_id();
+      $this->db->cache_delete('default', 'index');
+
+      return $lastId;
+    endif;
+  }
+
+  public function all()
+  {
+    $query = $this->db->get('lessees');
+
+    return $query;
+  }
+
+  public function insert()
+  {
+    $data['lessee_fname']   = $this->getFname();
+    $data['lessee_lname']   = $this->getLname();
+    $data['lessee_email']   = $this->getEmail();
+    $data['lessee_phoneno'] = $this->getPhoneno();
+    $data['username']       = $this->getUsername();
+    $data['password']       = $this->getPassword();
+
+    try {
+
+      $this->db->set($data);
+      $this->db->insert($this->lessees_table);
+      $this->db->cache_delete('lessee', 'profile');
+
+    }
+    catch (Exception $err) {
+
+      die($err->getMessage());
+
     }
 
-    public function setId($id)
-    {
-        $this->id = $id;
+    return true;
+  }
+
+  public function updateInfo()
+  {
+    $data['lessee_fname']   = $this->getFname();
+    $data['lessee_lname']   = $this->getLname();
+    $data['lessee_email']   = $this->getEmail();
+    $data['lessee_phoneno'] = $this->getPhoneno();
+
+    try {
+
+      $this->db->where('lessee_id', $this->getId());
+      $this->db->update($this->lessees_table, $data);
+      $this->db->cache_delete('lessee', 'profile');
+
+    }
+    catch (Exception $err) {
+
+      die($err->getMessage());
+
     }
 
-    private function getFname()
-    {
-        return $this->fname;
+    return true;
+  }
+
+  public function updateAccount()
+  {
+    $data['username'] = $this->getUsername();
+    $data['password'] = $this->getPassword();
+
+    try {
+
+      $this->db->where('lessee_id', $this->getId());
+      $this->db->update($this->lessees_table, $data);
+      $this->db->cache_delete('lessee', 'profile');
+
+    }
+    catch (Exception $err) {
+
+      die($err->getMessage());
+
     }
 
-    public function setFname($fname)
-    {
-        $this->fname = $fname;
+
+    return true;
+  }
+
+  public function delete()
+  {
+
+    try {
+
+      $this->db->where('lessee_id', $this->getId());
+      $this->db->delete($this->lessees_table);
+
+    }
+    catch (Exception $err) {
+
+      die($err->getMessage());
+
     }
 
-    private function getLname()
-    {
-        return $this->lname;
+    return true;
+  }
+
+  public function findByUsername()
+  {
+    $query = $this->db->get_where($this->lessees_table, array(
+      'username' => $this->getUsername()
+    ));
+
+    return $query->row_array();
+  }
+
+  public function findById()
+  {
+    $query = $this->db->get_where($this->lessees_table, array(
+      'lessee_id' => $this->getId()
+    ));
+
+    return $query->row_array();
+  }
+
+  public function findByEmail()
+  {
+    $query = $this->db->get_where($this->lessees_table, array(
+      'lessee_email' => $this->getEmail()
+    ));
+
+    return $query->row_array();
+  }
+
+  public function checkPassword()
+  {
+    $user  = $this->findById();
+    $query = $this->db->get_where($this->lessees_table, array(
+      'password' => $user['password']
+    ));
+    if ($this->getPassword() == $this->encrypt->decode($query->row()->password)):
+      return true;
+    else:
+      return false;
+    endif;
+  }
+
+  public function uploadImage()
+  {
+    $data['image'] = $this->getImage();
+
+    try {
+
+      $this->db->where('lessee_id', $this->getId());
+      $this->db->update($this->lessees_table, $data);
+      $this->db->cache_delete('lessee', 'profile');
+
+    }
+    catch (Exception $err) {
+
+      die($err->getMessage());
+
     }
 
-    public function setLname($lname)
-    {
-        $this->lname = $lname;
-    }
+    return true;
+  }
 
-    private function getEmail()
-    {
-        return $this->email;
-    }
+  public function myInterests()
+  {
+    $query  = $this->db->get_where($this->myinterests_table, array(
+      'lessee_id' => $this->getId()
+    ), $this->limit, $this->offset);
+    $result = $query->result();
 
-    public function setEmail($email)
-    {
-        $this->email = $email;
-    }
-
-    private function getPhoneno()
-    {
-        return $this->phoneno;
-    }
-
-    public function setPhoneno($phoneno)
-    {
-        $this->phoneno = $phoneno;
-    }
-
-    private function getUsername()
-    {
-        return $this->username;
-    }
-
-    public function setUsername($username)
-    {
-        $this->username = $username;
-    }
-
-    private function getPassword()
-    {
-        return $this->password;
-    }
-
-    public function setPassword($password)
-    {
-        $this->password = $password;
-    }
-
-    private function getImage()
-    {
-        return $this->image;
-    }
-
-    public function setImage($image)
-    {
-        $this->image = $image;
-    }
-
-    public function authenticate()
-    {
-        $user = $this->findByUsername($this->getUsername());
-
-        if (!empty($user) && $this->encrypt->decode($user['password']) == $this->getPassword()):
-            return $user;
-        else:
-            return false;
-        endif;
-    }
-
-    public function googleLogin($user)
-    {
-        $this->setUsername($user['id']);
-        $result = $this->findByUsername($this->getUsername());
-
-        if (!empty($result)):
-            $this->db->cache_delete('default', 'index');
-            return $result;
-        else:
-            $data['lessee_fname']   = $user['givenName'];
-            $data['lessee_lname']   = $user['familyName'];
-            $data['lessee_email']   = $user['email'];
-            $data['lessee_phoneno'] = "";
-            $data['username']       = $user['id'];
-            $data['password']       = "";
-            $this->db->set($data);
-            $this->db->insert($this->lessees_table);
-            $lastId = $this->db->insert_id();
-            $this->db->cache_delete('default', 'index');
-
-            return $lastId;
-        endif;
-    }
-
-    public function all()
-    {
-        $query = $this->db->get('lessees');
-
-        return $query;
-    }
-
-    public function insert()
-    {
-        $data['lessee_fname']   = $this->getFname();
-        $data['lessee_lname']   = $this->getLname();
-        $data['lessee_email']   = $this->getEmail();
-        $data['lessee_phoneno'] = $this->getPhoneno();
-        $data['username']       = $this->getUsername();
-        $data['password']       = $this->getPassword();
-        $this->db->set($data);
-        $this->db->insert($this->lessees_table);
-        $this->db->cache_delete('lessee', 'profile');
-
-        return true;
-    }
-
-    public function updateInfo()
-    {
-        $data['lessee_fname']   = $this->getFname();
-        $data['lessee_lname']   = $this->getLname();
-        $data['lessee_email']   = $this->getEmail();
-        $data['lessee_phoneno'] = $this->getPhoneno();
-        $this->db->where('lessee_id', $this->getId());
-        $this->db->update($this->lessees_table, $data);
-        $this->db->cache_delete('lessee', 'profile');
-
-        return true;
-    }
-
-    public function updateAccount()
-    {
-        $data['username'] = $this->getUsername();
-        $data['password'] = $this->getPassword();
-        $this->db->where('lessee_id', $this->getId());
-        $this->db->update($this->lessees_table, $data);
-        $this->db->cache_delete('lessee', 'profile');
-
-        return true;
-    }
-
-    public function delete()
-    {
-        $this->db->where('lessee_id', $this->getId());
-        $this->db->delete($this->lessees_table);
-
-        return true;
-    }
-
-    public function findByUsername()
-    {
-        $query = $this->db->get_where($this->lessees_table, array(
-            'username' => $this->getUsername()
-        ));
-
-        return $query->row_array();
-    }
-
-    public function findById()
-    {
-        $query = $this->db->get_where($this->lessees_table, array(
-            'lessee_id' => $this->getId()
-        ));
-
-        return $query->row_array();
-    }
-
-    public function findByEmail()
-    {
-        $query = $this->db->get_where($this->lessees_table, array(
-            'lessee_email' => $this->getEmail()
-        ));
-
-        return $query->row_array();
-    }
-
-    public function checkPassword()
-    {
-        $user  = $this->findById();
-        $query = $this->db->get_where($this->lessees_table, array(
-            'password' => $user['password']
-        ));
-        if ($this->getPassword() == $this->encrypt->decode($query->row()->password)):
-            return true;
-        else:
-            return false;
-        endif;
-    }
-
-    public function uploadImage()
-    {
-        $data['image'] = $this->getImage();
-        $this->db->where('lessee_id', $this->getId());
-        $this->db->update($this->lessees_table, $data);
-        $this->db->cache_delete('lessee', 'profile');
-
-        return true;
-    }
-
-    public function myInterests()
-    {
-        $query  = $this->db->get_where($this->myinterests_table, array(
-            'lessee_id' => $this->getId()
-        ), $this->limit, $this->offset);
-        $result = $query->result();
-
-        return $result;
-    }
+    return $result;
+  }
 
 }
--- application/models/MyShop.php
+++ application/models/MyShop.php
@@ -1,6 +1,7 @@
 <?php
 
-class MyShop extends CI_Model{
+class MyShop extends CI_Model
+{
 
   private $id;
   private $myshopname;
@@ -10,32 +11,67 @@ class MyShop extends CI_Model{
   private $limit = 20;
   private $offset = 0;
 
-  private function getId(){ return $this->id; }
+  private function getId()
+  {
+    return $this->id;
+  }
 
-  public function setId($id){ $this->id = $id; }
+  public function setId($id)
+  {
+    $this->id = $id;
+  }
 
-  private function getMyShopName(){ return $this->myshopname; }
+  private function getMyShopName()
+  {
+    return $this->myshopname;
+  }
 
-  public function setMyShopName($myshopname){ $this->myshopname = $myshopname; }
+  public function setMyShopName($myshopname)
+  {
+    $this->myshopname = $myshopname;
+  }
 
-  private function getLesseeId(){ return $this->lessee_id; }
+  private function getLesseeId()
+  {
+    return $this->lessee_id;
+  }
 
-  public function setLesseeId($lessee_id){ $this->lessee_id = $lessee_id; }
+  public function setLesseeId($lessee_id)
+  {
+    $this->lessee_id = $lessee_id;
+  }
 
-  private function getShopId(){ return $this->shop_id; }
+  private function getShopId()
+  {
+    return $this->shop_id;
+  }
 
-  public function setShopId($shop_id){ $this->shop_id = $shop_id; }
+  public function setShopId($shop_id)
+  {
+    $this->shop_id = $shop_id;
+  }
 
 
 
   public function insert()
   {
     $data['myshop_name'] = $this->getMyShopName();
-    $data['lessee_id'] = $this->getLesseeId();
-    $data['shop_id'] = $this->getShopId();
-    $this->db->set($data);
-    $this->db->insert($this->my_shops_table);
-    $this->db->cache_delete('lessee','myshops');
+    $data['lessee_id']   = $this->getLesseeId();
+    $data['shop_id']     = $this->getShopId();
+
+    try {
+
+      $this->db->set($data);
+      $this->db->insert($this->my_shops_table);
+      $this->db->cache_delete('lessee', 'myshops');
+
+    }
+    catch (Exception $err) {
+
+      die($err->getMessage());
+
+    }
+
 
     return true;
   }
@@ -45,7 +81,7 @@ class MyShop extends CI_Model{
     $this->db->select('rental_shops.shop_id, rental_shops.shop_name, rental_shops.shop_branch, rental_shops.subscriber_id, rental_shops.address, my_shops.myshop_id');
     $this->db->from('rental_shops');
     $this->db->join($this->my_shops_table, 'my_shops.shop_id = rental_shops.shop_id');
-    $query = $this->db->get();
+    $query  = $this->db->get();
     $result = $query->result();
 
     return $result;
@@ -53,20 +89,33 @@ class MyShop extends CI_Model{
 
   public function getMyShopsId()
   {
-    $query = $this->db->get_where($this->my_shops_table, array('lessee_id' => $this->getLesseeId()), $this->limit, $this->offset);
+    $query  = $this->db->get_where($this->my_shops_table, array(
+      'lessee_id' => $this->getLesseeId()
+    ), $this->limit, $this->offset);
     $result = array();
-    foreach($query->result_array() as $row):
-        $result[] = $row['shop_id'];
+    foreach ($query->result_array() as $row):
+      $result[] = $row['shop_id'];
     endforeach;
-    $this->db->cache_delete('lessee','shops');
+    $this->db->cache_delete('lessee', 'shops');
     return array_values($result);
   }
 
   public function delete()
   {
-    $this->db->where('myshop_id', $this->getId());
-    $this->db->delete($this->my_shops_table);
-    $this->db->cache_delete('lessee','myshops');
+
+    try {
+
+      $this->db->where('myshop_id', $this->getId());
+      $this->db->delete($this->my_shops_table);
+      $this->db->cache_delete('lessee', 'myshops');
+
+    }
+    catch (Exception $err) {
+
+      die($err->getMessage());
+
+    }
+
 
     return TRUE;
   }
