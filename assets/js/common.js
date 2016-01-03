2fc2997ac3f47d4101a96d39e6850ff620e976d3
Nichole Martinez <xin_martinez@yahoo.com>
2016-01-04 00:50:52 +0800

    Add previewImage on Common js

--- assets/js/common.js
+++ assets/js/common.js
@@ -13,3 +13,21 @@ function errorMessage(message) {
         type: 'error'
     });
 }
+
+// Preview image
+function previewImage(input) {
+  if (input.files && input.files[0]) {
+      var sizeMB = input.files[0].size/1024/1024;
+      if (sizeMB > 0.5) {
+        errorMessage("Image Size exceeds the limit. Please try again.");
+        $(input).val('');
+      } else {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+          $('#preview-image').fadeIn('fast');
+          $('#preview-image').attr('src', e.target.result);
+        }
+        reader.readAsDataURL(input.files[0]);
+      }
+  }
+}
