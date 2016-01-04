025a208c58e530c2790b20b79ff447cf8f546147
Gian Carlo Tomakin <gctomakin@fixerinit.co.jp>
2016-01-04 23:12:04 +0800

    Added items script

--- assets/js/pages/lessees/items.js
+++ assets/js/pages/lessees/items.js
@@ -1,3 +1,36 @@
 $(document).ready(function(){
-alert('TODO');
+  $('.my-interest-trigger').click(function(e) {
+    e.preventDefault();
+    var item_id = $(this).data('item-id');
+    var interest_name = $(this).data('interest-name');
+    var action = this.href;
+    var message = $('#message');
+    var el = $(this);
+    if (el.is('[disabled=disabled]')) {
+      return false;
+    }
+    $.post(action, {
+        item_id: item_id,
+        interest_name: interest_name
+      })
+      .done(function(data) {
+
+        if (data) {
+          el.attr("disabled", true);
+          $(".fa-plus-circle").text(" Added");
+          message.fadeIn(2000, function() {
+            $("html, body").animate({
+              scrollTop: $('body').offset().top
+            }, 100);
+            message.delay(2000).fadeOut();
+          });
+        } else {
+          alert('somethings wong..');
+        }
+
+      })
+      .fail(function(xhr, textStatus, errorThrown) {
+        throw new Error(xhr.responseText);
+      });
+  });
 });
