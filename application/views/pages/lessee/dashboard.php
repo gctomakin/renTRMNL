76d5b1f4be29c1718bbb792ad022aaa337bb0d64
Gian Carlo Tomakin <gctomakin@fixerinit.co.jp>
2016-01-11 00:04:29 +0800

    Modified html

--- application/views/pages/lessee/dashboard.php
+++ application/views/pages/lessee/dashboard.php
@@ -1,23 +1,24 @@
 <div class="container-fluid">
         <div class="row no-gutter">
+        <?php if(empty($categories)): ?>
+          <div class="row no-gutter">
+            <div class="col-md-12">
+                <div class="alert alert-warning" role="alert"> <strong>Oops!</strong> Better check later, no categories added yet. </div>
+            </div>
+          </div>
+        <?php else: ?>
+          <?php foreach($categories as $category):?>
             <div class="col-lg-4 col-sm-6 portfolio-item">
                 <a href="<?php echo site_url('lessee/shops'); ?>" class="portfolio-box">
                   <img src="http://localhost/rentrmnl/assets/img/portfolio/1.jpg" class="img-responsive" alt="">
                   <h3>
-                    <a href="<?php echo site_url('lessee/shops'); ?>">Shops</a>
+                    <a href="<?php echo site_url('lessee/shops/'); ?>"></a><?php echo $category->category_type;?></a>
                   </h3>
                 </a>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
             </div>
-            <div class="col-lg-4 col-sm-6 portfolio-item">
-                <a href="<?php echo site_url('lessee/items'); ?>" class="portfolio-box">
-                  <img src="http://localhost/rentrmnl/assets/img/portfolio/2.jpg" class="img-responsive" alt="">
-                  <h3>
-                    <a href="<?php echo site_url('lessee/items'); ?>">Items</a>
-                  </h3>
-                </a>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-            </div>
+          <?php endforeach;?>
+        <?php endif; ?>
         </div>
         <!--pagination-->
         <div class="row text-center">
