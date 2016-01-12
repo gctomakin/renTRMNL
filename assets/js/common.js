22b3d0a645c5101a665c552d204cad06c48c41a3
Nichole Martinez <xin_martinez@yahoo.com>
2016-01-13 00:22:32 +0800

    Add function in common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -31,3 +31,18 @@ function previewImage(input) {
       }
   }
 }
+
+function formatNumber(number, decimalsLength, decimalSeparator, thousandSeparator) {
+  var n = number,
+     decimalsLength = isNaN(decimalsLength = Math.abs(decimalsLength)) ? 2 : decimalsLength,
+     decimalSeparator = decimalSeparator == undefined ? "." : decimalSeparator,
+     thousandSeparator = thousandSeparator == undefined ? "," : thousandSeparator,
+     sign = n < 0 ? "-" : "",
+     i = parseInt(n = Math.abs(+n || 0).toFixed(decimalsLength)) + "",
+     j = (j = i.length) > 3 ? j % 3 : 0;
+
+  return sign +
+     (j ? i.substr(0, j) + thousandSeparator : "") +
+     i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousandSeparator) +
+     (decimalsLength ? decimalSeparator + Math.abs(n - i).toFixed(decimalsLength).slice(2) : "");
+}
