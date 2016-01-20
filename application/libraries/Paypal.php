20e53dc10e463a8a6898ca4481128b54aaf2cb43
Nichole Martinez <xin_martinez@yahoo.com>
2016-01-21 07:12:21 +0800

    Fix Paypal class, change curl SSL version

--- application/libraries/Paypal.php
+++ application/libraries/Paypal.php
@@ -35,16 +35,25 @@ class Paypal {
 	}
 
 	private function _paypalSend($data, $call) {
-
 		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1);
 		curl_setopt($ch, CURLOPT_URL, $this->apiUrl.$call);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $this->_headers());
-
-		return json_decode(curl_exec($ch), TRUE);
+		// curl_setopt($ch, CURLOPT_VERBOSE, TRUE);
+  //  	$verbose = fopen('php://temp', 'w+');
+		// curl_setopt($ch, CURLOPT_STDERR, $verbose);
+		// $result = curl_exec($ch);
+		// if ($result === FALSE) {
+		// 	printf("cUrl error (#%d): %s<br>\n", curl_errno($ch),
+  //          htmlspecialchars(curl_error($ch)));
+		// } else {
+		// 	return json_encode($result, TRUE);
+		// }
+   	return json_decode(curl_exec($ch), TRUE);
 	}
 
 	private function _headers() {
