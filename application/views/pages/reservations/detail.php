bd4ea9c72afa15b6925c7856b0b362f8410ac3d7
Nichole Martinez <xin_martinez@yahoo.com>
2016-01-27 06:16:20 +0800

    Fix reservation detail

--- application/views/pages/reservations/detail.php
+++ application/views/pages/reservations/detail.php
@@ -3,21 +3,16 @@
     <?php foreach($details as $detail):?>
       <div class="col-sm-3 col-lg-3 col-md-3">
         <div class="thumbnail">
-        	<?php 
-        		$itemPic = $detail->item_pic == NULL ? 
-	        		'http://placehold.it/320x150' : 
-	        		'data:image/jpeg;base64,'.base64_encode($detail->item_pic);
-	        ?>
-          <img src="<?php echo $itemPic; ?>" alt="">
+        	<img src="<?php echo $detail['item_pic']; ?>" alt="<?php echo $detail['item_desc']; ?>" style="width:250; height:150px;">
           <div class="caption">
-              <h4><a href="#"><?php echo $detail->item_desc; ?></a></h4>
+              <h4><a href="#"><?php echo $detail['item_desc']; ?></a></h4>
               <dl>
                 <dt>Item Rate</dt>
-                <dd><?php echo number_format($detail->item_rate, 2); ?></dd>
+                <dd><?php echo number_format($detail['item_rate'], 2); ?></dd>
                 <dt>Quantity</dt>
-                <dd><?php echo $detail->qty; ?></dd>
+                <dd><?php echo $detail['qty']; ?></dd>
                 <dt>TOTAL</dt>
-                <dd><?php echo number_format($detail->qty * $detail->item_rate, 2); ?></dd>
+                <dd><?php echo number_format($detail['qty'] * $detail['item_rate'], 2); ?></dd>
               </dl>
           </div>
         </div>
