d7f10a146e60124f8b559966ba6a915c15913e56
Nichole Martinez <xin_martinez@yahoo.com>
2016-01-27 20:12:56 +0800

    Fix lessee item list info with categories

--- application/views/pages/lessee/categories/items.php
+++ application/views/pages/lessee/categories/items.php
@@ -7,34 +7,42 @@
     <div id="message" class="alert alert-success text-center" hidden><strong>Successfully</strong> Added to your My Interests list.</div>
     <?php if(empty($items)): ?>
       <div class="col-md-12">
-        <div class="alert alert-warning" role="alert"> <strong>Oops!</strong> Better check later, no items added yet. </div>
+        <div class="alert alert-warning" role="alert"> <strong>Oops!</strong> No item found. </div>
       </div>
     <?php else: ?>
       <?php foreach($items as $item):?>
         <div class="col-sm-3 col-lg-3 col-md-3">
             <div class="thumbnail">
             <?php
-              $itemPic = $item->item_pic == NULL ? 
+              $itemPic = $item['info']->item_pic == NULL ? 
                         'http://placehold.it/320x150' :
-                        'data:image/jpeg;base64,'.base64_encode($item->item_pic);
+                        'data:image/jpeg;base64,'.base64_encode($item['info']->item_pic);
             ?>
                 <img src="<?php echo $itemPic; ?>" alt="" style="width:320px; height:150px;">
                 <div class="caption">
-                    <h4><a href="#"><?php echo $item->item_desc; ?></a></h4>
-                    <dl>
-                      <dt>Item Rate</dt>
-                      <dd><?php echo $item->item_rate; ?></dd>
-                      <dt>Item Qty</dt>
-                      <dd><?php echo $item->item_qty; ?></dd>
-                      <dt>Shop Name</dt>
-                      <dd><?php echo $item->shop_name; ?></dd>
-                      <dt>Shop Branch</dt>
-                      <dd><?php echo $item->shop_branch; ?></dd>
-                    </dl>
+                    <h4><a href="#"><?php echo $item['info']->item_desc; ?></a></h4>
+                    <p>₱ <?php echo number_format($item['info']->item_rate, 2) . '/pcs'; ?></p>
+                    <?php if (isset($item['info']->shop_name)) { ?>
+                    <p><?php echo $item['info']->shop_name . ' - ' . $item['info']->shop_branch; ?></p>
+                    <?php } ?>
+                    <p>
+                      <?php 
+                        if (!empty($item['categories'])) {
+                          $label = "<span class='label label-default'>";
+                          echo $label;
+                          $categoryType = array();
+                          foreach ($item['categories'] as $category) {
+                            $categoryType[] = $category->category_type;
+                          }
+                          echo implode("</span>  $label", $categoryType);
+                          echo "</span>";
+                        }
+                      ?>
+                    </p>
                     <div class="btn-group">
-                      <a href="<?php echo site_url('reservations/item/' . $item->item_id); ?>" class="btn btn-info btn-xs">Reserve</a>
-                      <button class="btn btn-success btn-xs btn-rent" data-item-id="<?php echo $item->item_id; ?>">Rent</button>
-                      <a class="btn btn-primary btn-xs my-interest-trigger" data-item-id="<?php echo $item->item_id; ?>" data-interest-name="<?php echo $item->item_desc; ?>" href="<?php echo $action; ?>" <?php echo (in_array($item->item_id,$myinterests)) ? 'disabled=disabled' : ''; ?>><span class="fa fa-plus-circle"> <?php echo (in_array($item->item_id,$myinterests)) ? 'Added ' : 'My Interest '; ?></span></a>
+                      <a href="<?php echo site_url('reservations/item/' . $item['info']->item_id); ?>" class="btn btn-info btn-xs">Reserve</a>
+                      <button class="btn btn-success btn-xs btn-rent" data-item-id="<?php echo $item['info']->item_id; ?>">Rent</button>
+                      <a class="btn btn-primary btn-xs my-interest-trigger" data-item-id="<?php echo $item['info']->item_id; ?>" data-interest-name="<?php echo $item['info']->item_desc; ?>" href="<?php echo $action; ?>" <?php echo (in_array($item['info']->item_id,$myinterests)) ? 'disabled=disabled' : ''; ?>><span class="fa fa-plus-circle"> <?php echo (in_array($item['info']->item_id,$myinterests)) ? 'Added ' : 'My Interest '; ?></span></a>
                     </div>
                 </div>
             </div>
