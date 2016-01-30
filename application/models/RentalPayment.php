3f323f50e8562a091a47155506b141507300a83b
Nichole Martinez <xin_martinez@yahoo.com>
2016-01-30 08:41:44 +0800

    Add model RentalPayment

---
+++ application/models/RentalPayment.php
@@ -0,0 +1,113 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class RentalPayment extends CI_Model{
+
+	private $table = "rental_payments";
+
+	private $id = "payment_id";
+	private $amount = "payment_amt";
+	private $date = "payment_date";
+	private $reserveId = "reserve_id";
+	private $status = "payment_status";
+
+	private $data = array(
+		"payment_id" => "",
+		"payment_amt" => "",
+		"payment_date" => "",
+		"reserve_id" => "",
+		"payment_status" => ""
+	);
+
+	public function clean() {
+		foreach($this->data as $key => $value) {
+			$this->data[$key] = "";
+		}
+	}
+
+	public function create() {
+		$data = array_filter($this->data);
+		$this->db->insert($this->table, $data);
+		$this->deleteCache();
+		return $this->db->insert_id();
+	}
+
+	public function update() {
+		$data = array_filter($this->data);
+		$this->db->update($this->table, $data, array(
+				$this->id => $data[$this->id]
+			)
+		);
+		$this->deleteCache();
+		return $this->db->affected_rows();
+	}
+
+	public function delete($id) {
+		$this->db->delete($this->table, array(
+				$this->id => $id
+			)
+		);
+		$this->deleteCache();
+		return $this->db->affected_rows();	
+	}
+
+	public function all() {
+		$query = $this->db->get($this->table);
+		return $query->result();
+	}
+
+	public function find() {
+		$data = array_filter($this->data);
+		$query = $this->db->get_where($this->table, $data);
+		return $query->result();
+	}
+
+	public function findById($id) {
+		$query = $this->db->get_where($this->table, array($this->id => $id));
+		return $query->row_array();
+	}
+
+	public function findPendingByLessorId($lessorId, $status = "") {
+		$joinRes = $this->_joinReservation();
+		$joinCon = " AND " . $this->rAlias . "." . $this->Reservation->getSubscriberId() . " = $lessorId";
+		if (!empty($status)) {
+			$this->db->where($this->status, $status);
+		}
+		$query = $this->db
+			->from($this->table . ' as ' . $this->rpAlias)
+			->join($joinRes['table'], $joinRes['on'] . $joinCon, 'INNER')
+			->get();
+
+		return $query->result();
+		
+	}
+
+	private function _joinReservation() {
+		$this->load->model('Reservation');
+		$table = $this->Reservation->getTable() . ' as ' . $this->rAlias;
+		$on = $this->rAlias . '.' . $this->Reservation->getId() . ' = ';
+		$on .= $this->rpAlias . '.' . $this->reserveId;
+
+		return array('table' => $table, 'on' => $on);
+	}
+
+	private $rAlias ="r";
+	private $rpAlias ="rp";
+
+	// GETTERS AND SETTERS
+	public function getId() { $this->id; }
+	public function getAmount() { $this->amount; }
+	public function getDate() { $this->date; }
+	public function getReserveId() { $this->reserveId; }
+	public function getTable() { $this->table; }
+	public function getStatus() { $this->status; }
+
+	public function setId($value) { $data[$this->id] = $value; }
+	public function setAmount($value) { $data[$this->amount] = $value; }
+	public function setDate($value) { $data[$this->date] = $value; }
+	public function setReserveId($value) { $data[$this->reserveId] = $value; }
+	public function setStatus($value) { $data[$this->status] = $value; }
+	
+	private function deleteCache() {
+	
+	}
+}
