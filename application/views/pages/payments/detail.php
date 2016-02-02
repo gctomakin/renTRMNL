82bbd1255064b5328c5cd0c5ed5ab22b31f216dc
Nichole Martinez <xin_martinez@yahoo.com>
2016-02-02 09:45:35 +0800

    Add view payments detail

---
+++ application/views/pages/payments/detail.php
@@ -0,0 +1,44 @@
+<table class="table">
+	<thead>
+		<tr>
+			<th>DATE</th>
+			<th>DESCRIPTION</th>
+			<th class="text-right">AMOUNT</th>
+			<th>STATUS</th>
+			<th></th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach ($payments as $payment) { ?>
+		<tr data-pay-id="<?php echo $payment->payment_id; ?>">
+			<td><?php echo date('M d, Y H:i a', strtotime($payment->payment_date)); ?></td>
+			<td><?php echo $payment->payment_description; ?></td>
+			<td class="text-right"><?php echo $payment->payment_amt; ?></td>
+			<td class="status"><?php echo $payment->payment_status; ?></td>
+			<td>
+				<?php if ($payment->payment_status == 'pending') { ?>
+					<?php if (isset($isLessor)) { ?>
+				<button class="btn btn-xs btn-primary btn-pay-receive">receive</button>	
+					<?php }?>
+				<button class="btn btn-xs btn-primary btn-pay-cancel">cancel</button>
+				<?php } ?>
+				<input type="hidden" class="pay-id" value="<?php echo $payment->payment_id ?>">
+			</td>
+		</tr>
+		<?php } ?>
+	</tbody>
+</table>
+<script type="text/javascript">
+	var cancelUrl = "<?php echo $cancelUrl; ?>";
+	$(document).ready(function() {
+		$('.btn-pay-cancel').click(function() {
+			var button = $(this);
+			var id = button.siblings('.pay-id').val();
+			
+			proccessAction(cancelUrl, {id: id}).then(function() {
+				$('[data-pay-id="'+id+'"]').find('.status').text('cancel');
+				button.hide();
+			});
+		});
+	}); 
+</script>
