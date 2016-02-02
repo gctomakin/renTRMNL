53c319472d5329ccac2acdcd4a39d2cc5158d8b6
Nichole Martinez <xin_martinez@yahoo.com>
2016-02-03 05:24:44 +0800

    Add lessor pending page

---
+++ application/views/pages/lessor/pending.php
@@ -0,0 +1,8 @@
+<div class="col-lg-12">
+	<h1>
+		PLEASE WAIT WHILE YOUR SUBSCRIPTION IS ON PROCESS.
+	</h1>
+	<p>
+		Report to <a href="#">ADMIN</a> if your subscription is idle for 24 hours already.
+	</p>
+</div>
