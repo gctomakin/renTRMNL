972e12d4498da5e23fc8c7ebc66cb135d1239bfc
Nichole Martinez <xin_martinez@yahoo.com>
2016-02-10 23:58:09 +0800

    Fix lessor's rental history

---
+++ application/views/pages/items/report.php
@@ -0,0 +1,34 @@
+<div class="container-fluid">
+	<div class="row no-gutter">
+		<div class="col-lg-12">
+			<a href="<?php echo site_url('lessor/rental/history') ?>"><i class="fa fa-tag"></i> Go to Rental history</a>
+		</div>
+		<hr>
+		<div class="col-lg-12">
+			<table id="item-report-table" class="table">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>DESCRIPTION</th>
+						<th>SHOP</th>
+						<th>RENTED</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php foreach ($items as $item) {
+						$shop = empty($item->shop_name) ? 'No shop' : $item->shop_name . ' - ' . $item->shop_branch;
+					?>
+					<tr>
+						<td><?php echo $item->item_id; ?></td>
+						<td><?php echo $item->item_desc; ?></td>
+						<td><?php echo $shop; ?></td>
+						<td><?php echo number_format($item->rented); ?></td>
+						<td></td>
+					</tr>
+					<?php } ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
--- application/views/pages/lessor/reservations/history.php
+++ application/views/pages/lessor/reservations/history.php
@@ -28,6 +28,7 @@
 					<td class="status"><?php echo $rev->status; ?></td>
 					<td class="text-center">
 						<div class="btn-group" role="group" aria-label="reservation-options">
+							<button class="btn btn-xs btn-default btn-view" data-rev-id="<?php echo $rev->reserve_id; ?>">view details</button>
 						</div>
 					</td>
 				</tr>
@@ -36,3 +37,22 @@
 		</table>
 	</div>
 </div>
+<div class="modal modal-fullscreen fade" id="reservation-modal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+				<h4 class="modal-title" id="reservation-modal-title" >Rerservation's details</i></h4>
+      </div>
+      <div class="modal-body"></div>
+			<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+<script type="text/javascript">
+	var reservationDetailUrl = "<?php echo site_url('reservations/detail'); ?>";
+</script>
