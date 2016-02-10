042b642cc79be62f2f588272ceb7193d0dd4e887
Nichole Martinez <xin_martinez@yahoo.com>
2016-02-10 20:25:23 +0800

    Add view on items listByCategory

---
+++ application/views/pages/items/listByCategory.php
@@ -0,0 +1,40 @@
+<div class="container-fluid">
+  <div class="row no-gutter">
+    <?php foreach($items as $item):?>
+    <div class="col-sm-3 col-lg-3 col-md-3">
+      <div class="thumbnail">
+        <img src="<?php echo $item['item_pic']; ?>" alt="" style="width:320px; height:150px;">
+        <div class="caption">
+          <h4><a href="#"><?php echo $item['item_desc']; ?></a></h4>
+          <p>₱ <?php echo number_format($item['item_rate'], 2); ?> 
+            <small>
+              <?php echo $item['mode_label']; ?>
+            </small>
+          </p>
+          <?php if (isset($item['shop_name'])) { ?>
+          <p><?php echo $item['shop_name'] . ' - ' . $item['shop_branch']; ?></p>
+          <?php } ?>
+          <p>
+            <?php 
+              if (!empty($item['categories'])) {
+                $label = "<span class='label label-default'>";
+                echo $label;
+                $categoryType = array();
+                foreach ($item['categories'] as $category) {
+                  $categoryType[] = $category->category_type;
+                }
+                echo implode("</span>  $label", $categoryType);
+                echo "</span>";
+              }
+            ?>
+          </p>
+        </div>
+      </div>
+    </div>
+    <?php endforeach; ?>
+    <!--pagination-->
+    <div class="text-center col-lg-12">
+      <?php echo empty($pagination) ? '' : $pagination; ?>
+    </div>
+  </div>
+</div>
