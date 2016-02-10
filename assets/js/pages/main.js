ff356f49124403ba7a371651299e5f43b2595b00
Nichole Martinez <xin_martinez@yahoo.com>
2016-02-10 20:25:41 +0800

    Add script on main js

--- assets/js/pages/main.js
+++ assets/js/pages/main.js
@@ -8,4 +8,30 @@ $(document).ready(function() {
     }
     $('#form-signin').attr('action', action);
   });
+
+  $('.portfolio-box').click(function() {
+  	catId = $(this).data('category-id');
+  	var catName = $(this).data('category-name');
+  	$('#category-modal').find('#category-modal-title').text(catName);
+  	viewCategory(catId, 1);
+  });
 });
+
+$(document).on('click', '.pagination a', function(e) {
+	e.preventDefault();
+	var page = $(this).data('ci-pagination-page');
+	viewCategory(catId, page);
+});
+
+var catId;
+
+function viewCategory(id, page) {
+	$.post(categoryItemsUrl+'/'+id+'/'+page, {}, function(data) {
+		if (data['result']) {
+			$('#category-modal').find('.modal-body').html(data['view']);
+			$('#category-modal').modal('show');
+		} else {
+			errorMessage(data['message']);
+		}
+	}, 'JSON');
+}
