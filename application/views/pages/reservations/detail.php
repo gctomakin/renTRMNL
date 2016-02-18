da3b58e7b5f347e500420689161336a3eb7cf794
Nichole Martinez <xin_martinez@yahoo.com>
2016-02-18 22:27:37 +0800

    Fix reservation detail item name

--- application/views/pages/reservations/detail.php
+++ application/views/pages/reservations/detail.php
@@ -5,7 +5,8 @@
         <div class="thumbnail">
         	<img src="<?php echo $detail['item_pic']; ?>" alt="<?php echo $detail['item_desc']; ?>" style="width:250; height:150px;">
           <div class="caption">
-              <h4><a href="#"><?php echo $detail['item_desc']; ?></a></h4>
+              <h4><a href="#"><?php echo empty($detail['item_name']) ? 'No Name' : $detail['item_name']; ?></a></h4>
+              <p><?php echo $detail['item_desc']; ?></p>
               <dl>
                 <dt>Rental Amount</dt>
                 <dd><?php echo number_format($detail['rental_amt'], 2); ?></dd>
