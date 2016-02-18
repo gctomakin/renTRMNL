faa05394e6ba8f219dc3703f5ee752b2c5199f20
Nichole Martinez <xin_martinez@yahoo.com>
2016-02-18 22:19:40 +0800

    Fix lessors item list view

--- application/views/pages/items/list.php
+++ application/views/pages/items/list.php
@@ -1,59 +1,62 @@
 <div class="container-fluid">
-	<!-- <div class="row no-gutter"> -->
-	<?php foreach($items as $item) { ?>
-	<section data-item-list-id="<?php echo $item['info']->item_id; ?>">
-    <div class="row no-gutter">
-  		<div class="col-md-7 col-sm-7">
-        <a href="#">
-          <?php $itemPic = $item['info']->item_pic == NULL ? 'http://placehold.it/700x300' : 'data:image/jpeg;base64,'.base64_encode($item['info']->item_pic); ?>
-          <img class="thumbnail img-responsive" src="<?php echo $itemPic; ?>" alt="">
-        </a>
-      </div>
-      <div class="col-md-5 col-sm-5">
-        <h3><?php echo $item['info']->item_desc;?></h3>
-        <h4><?php echo $item['info']->shop_name . ' - ' . $item['info']->shop_branch; ?></h4>
-        <div class="row">
-        	<div class="col-md-6">
-        		RATE : <?php echo number_format($item['info']->item_rate, 2); ?> <br>
-  					CASH BOND : <?php echo number_format($item['info']->item_cash_bond, 2); ?> <br>
-        		RENTAL MODE : <?php echo $rental_modes[$item['info']->item_rental_mode]; ?>
-        	</div>
-        	<div class="col-md-6">
-  					PENALTY : <?php echo $item['info']->item_penalty; ?> <br>
-  					QUANTITY : <?php echo $item['info']->item_qty; ?>
-  				</div>
-  				<div class="col-md-12">
-  					<p>
-  						<?php
-                if (!empty($item['categories'])) { 
-    							$label = "<span class='label label-default'>";
-                  echo $label;
-                  $categoryType = array();
-    							foreach ($item['categories'] as $category) {
-    								$categoryType[] = $category->category_type;
-    							}
-    							echo implode("</span> $label", $categoryType);
-                  echo "</span>";
-                }
-  						?>
-  					</p>
-  				</div>
-  				<div class="col-md-12">
-  					<button class="btn btn-default btn-sm view-item-detail">View details</button>
-  					<a class="btn btn-success btn-sm" href="<?php echo site_url('lessor/items/edit/' . $item['info']->item_id); ?>">Edit</a>
-  	  			<button class="btn btn-sm btn-primary item-delete">Delete</button>
-            <input type="hidden" class="item-property" data-item-id="<?php echo $item['info']->item_id; ?>" data-item-description="<?php echo $item['info']->item_desc; ?>">
-  	      </div>
+	<?php if (empty($items)) { ?>
+    <h2 class="text-danger">No Item Exist.</h2>
+  <?php } else { ?>
+    <?php foreach($items as $item) { ?>
+  	<section data-item-list-id="<?php echo $item['info']->item_id; ?>">
+      <div class="row no-gutter">
+    		<div class="col-md-7 col-sm-7">
+          <a href="#">
+            <?php $itemPic = $item['info']->item_pic == NULL ? 'http://placehold.it/700x300' : 'data:image/jpeg;base64,'.base64_encode($item['info']->item_pic); ?>
+            <img class="thumbnail img-responsive" src="<?php echo $itemPic; ?>" alt="">
+          </a>
+        </div>
+        <div class="col-md-5 col-sm-5">
+          <h2><?php echo $item['info']->item_name;?></h2>
+          <h5><i class="fa fa-building"></i> <?php echo $item['info']->shop_name . ' - ' . $item['info']->shop_branch; ?></h5>
+          <p>(<?php echo $item['info']->item_desc; ?>)</p>
+          <div class="row">
+          	<div class="col-md-6">
+          		RATE : <?php echo number_format($item['info']->item_rate, 2); ?> <br>
+    					CASH BOND : <?php echo number_format($item['info']->item_cash_bond, 2); ?> <br>
+          		RENTAL MODE : <?php echo $rental_modes[$item['info']->item_rental_mode]; ?>
+          	</div>
+          	<div class="col-md-6">
+    					PENALTY : <?php echo $item['info']->item_penalty; ?> <br>
+    					QUANTITY : <?php echo $item['info']->item_qty; ?>
+    				</div>
+    				<div class="col-md-12">
+    					<p>
+    						<?php
+                  if (!empty($item['categories'])) { 
+      							$label = "<span class='label label-default'>";
+                    echo $label;
+                    $categoryType = array();
+      							foreach ($item['categories'] as $category) {
+      								$categoryType[] = $category->category_type;
+      							}
+      							echo implode("</span> $label", $categoryType);
+                    echo "</span>";
+                  }
+    						?>
+    					</p>
+    				</div>
+    				<div class="col-md-12">
+    					<button class="btn btn-default btn-sm view-item-detail">View details</button>
+    					<a class="btn btn-success btn-sm" href="<?php echo site_url('lessor/items/edit/' . $item['info']->item_id); ?>">Edit</a>
+    	  			<button class="btn btn-sm btn-primary item-delete">Delete</button>
+              <input type="hidden" class="item-property" data-item-id="<?php echo $item['info']->item_id; ?>" data-item-description="<?php echo $item['info']->item_name; ?>">
+    	      </div>
+          </div>
         </div>
       </div>
-    </div>
-    <hr>
-  </section>
+      <hr>
+    </section>
+    <?php } ?>
+  	<div class="text-center">
+  	<?php echo empty($pagination) ? '' : $pagination; ?>
+  	</div>
   <?php } ?>
-	<!-- </div> -->
-	<div class="text-center">
-	<?php echo empty($pagination) ? '' : $pagination; ?>
-	</div>
 </div>
 <div id="item-detail-modal" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
   <div class="modal-dialog modal-lg">
