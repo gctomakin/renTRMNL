7902aabc6ca92f1e6f07f4b914da37cb4ca3d8d1
Nichole Martinez <xin_martinez@yahoo.com>
2016-02-20 16:46:35 +0800

    Added description on list by Category

--- application/views/pages/items/listByCategory.php
+++ application/views/pages/items/listByCategory.php
@@ -6,38 +6,36 @@
     </div>
     <?php } else { ?>
       <?php foreach($shops as $shop):?>
-      <div class="col-sm-3 col-lg-3 col-md-3">
-        <div class="thumbnail">
-          <img src="<?php echo $shop['detail']['shop_image']; ?>" alt="" style="width:320px; height:150px;">
-          <div class="caption">
-            <h4>
-              <a href="#">
-                <?php echo $shop['detail']['shop_name'] . ' - ' . $shop['detail']['shop_name']; ?>
-              </a>
-            </h4>
-            <p><?php echo $shop['detail']['address']; ?></p>
-            <table class="table table-small">
-              <tr>
-                <th>ITEM</th>
-                <th>QTY</th>
-                <th class='text-right'>PRICE</th>
-              </tr>
-              <?php foreach ($shop['items'] as $item) { ?>
-              <tr>
-                <td>
-                  <a target="_blank" href="<?php echo site_url('/lessee/items?id=' . $item['item_id']); ?>">
-                    <?php echo $item['item_name']; ?>
-                    <small>(<?php echo $item['item_desc'] ?>)</small>
-                  </a>
-                </td>
-                <td><?php echo number_format($item['item_qty']); ?></td>
-                <td class='text-right'><?php echo number_format($item['item_rate'], 2); ?></td>
-              </tr>
-              <?php } ?>
-            </table>
-          </div>
-        </div>
+      <div class="col-md-5">
+        <a href="#">
+          <img class="thumbnail img-responsive" src="<?php echo $shop['detail']['shop_image']; ?>" alt="" style="width:100%; height: 250px;">
+        </a>
       </div>
+      <div class="col-md-7">
+        <h3><?php echo $shop['detail']['shop_name'] . ' - ' . $shop['detail']['shop_branch']; ?></h3>
+        <h4><?php echo $shop['detail']['address']; ?></h4>
+        <p><?php echo $shop['detail']['shop_desc']; ?></p>
+        <table class="table">
+          <tr>
+            <th>ITEM</th>
+            <th>QTY</th>
+            <th class='text-right'>PRICE</th>
+          </tr>
+          <?php foreach ($shop['items'] as $item) { ?>
+          <tr>
+            <td>
+              <a target="_blank" href="<?php echo site_url('/lessee/items?id=' . $item['item_id']); ?>">
+                <?php echo $item['item_name']; ?>
+                <small>(<?php echo $item['item_desc'] ?>)</small>
+              </a>
+            </td>
+            <td><?php echo number_format($item['item_qty']); ?></td>
+            <td class='text-right'><?php echo number_format($item['item_rate'], 2); ?></td>
+          </tr>
+          <?php } ?>
+        </table>
+      </div>
+      <div class="col-md-12"><hr></div>
       <?php endforeach; ?>
       <!--pagination-->
       <div class="text-center col-lg-12">
