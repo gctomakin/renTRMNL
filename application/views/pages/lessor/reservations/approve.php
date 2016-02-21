687bda26db7725b29520fe6f92324dbafeb8251a
Nichole Martinez <xin_martinez@yahoo.com>
2016-02-21 20:29:33 +0800

    Fix lessor reservations approve view

--- application/views/pages/lessor/reservations/approve.php
+++ application/views/pages/lessor/reservations/approve.php
@@ -4,19 +4,23 @@
 			<thead>
 				<tr>
 					<th>ID</th>
+					<th>LESSEE</th>
+					<th>SHOP</th>
 					<th>DATE</th>
 					<th>DURATION</th>
 					<th class="text-right">TOTAL</th>
 					<th class="text-right">PENALTY</th>
 					<th class="text-right">BALANCE</th>
 					<th>STATUS</th>
-					<th class="text-center">OPTION</th>
+					<th class="text-center" width="100">OPTION</th>
 				</tr>
 			</thead>
 			<tbody>
 				<?php foreach($reservations as $rev) { ?>
 				<tr data-reservation="<?php echo $rev->reserve_id; ?>">
 					<td><?php echo $rev->reserve_id; ?></td>
+					<td><?php echo $rev->lessee_fname; ?></td>
+					<td><?php echo $rev->shop_name ; ?></td>
 					<td><?php echo date('M d, Y', strtotime($rev->reserve_date)); ?></td>
 					<td>
 						<?php 
@@ -40,7 +44,7 @@
 							<?php
 								}
 							?>
-							<button class="btn btn-default btn-xs btn-view" data-rev-id="<?php echo $rev->reserve_id?>">view details</button>
+							<button class="btn btn-default btn-xs btn-view" data-rev-id="<?php echo $rev->reserve_id?>">details</button>
 							<button class="btn btn-danger btn-xs btn-payment" data-rev-id="<?php echo $rev->reserve_id?>">payments</button>
 						</div>
 					</td>
