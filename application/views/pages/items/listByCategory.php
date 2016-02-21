d8be76e42feb803a14bf2baa45c38c1c3e8978c2
Nichole Martinez <xin_martinez@yahoo.com>
2016-02-21 23:35:15 +0800

    Add image on item list on listbyCategory

--- application/views/pages/items/listByCategory.php
+++ application/views/pages/items/listByCategory.php
@@ -15,24 +15,32 @@
         <h3><?php echo $shop['detail']['shop_name'] . ' - ' . $shop['detail']['shop_branch']; ?></h3>
         <h4><?php echo $shop['detail']['address']; ?></h4>
         <p><?php echo $shop['detail']['shop_desc']; ?></p>
-        <table class="table">
-          <tr>
-            <th>ITEM</th>
-            <th>QTY</th>
-            <th class='text-right'>PRICE</th>
-          </tr>
-          <?php foreach ($shop['items'] as $item) { ?>
-          <tr>
-            <td>
-              <a target="_blank" href="<?php echo site_url('/lessee/items?id=' . $item['item_id']); ?>">
-                <?php echo $item['item_name']; ?>
-                <small>(<?php echo $item['item_desc'] ?>)</small>
-              </a>
-            </td>
-            <td><?php echo number_format($item['item_qty']); ?></td>
-            <td class='text-right'><?php echo number_format($item['item_rate'], 2); ?></td>
-          </tr>
-          <?php } ?>
+        <table class="table table-hover list-category-table">
+          <thead>
+            <tr>
+              <th width="50">PIC</th>
+              <th>ITEM</th>
+              <th>QTY</th>
+              <th class='text-right'>PRICE</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php foreach ($shop['items'] as $item) { ?>
+            <tr>
+              <td>
+                <img src="<?php echo $item['item_pic']; ?>" alt="" style="width: 50px; height: 50px;">
+              </td>
+              <td>
+                <a target="_blank" href="<?php echo site_url('/lessee/items?id=' . $item['item_id']); ?>">
+                  <?php echo $item['item_name']; ?>
+                  <small>(<?php echo $item['item_desc'] ?>)</small>
+                </a>
+              </td>
+              <td><?php echo number_format($item['item_qty']); ?></td>
+              <td class='text-right'><?php echo number_format($item['item_rate'], 2); ?></td>
+            </tr>
+            <?php } ?>
+          </tbody>
         </table>
       </div>
       <div class="col-md-12"><hr></div>
@@ -44,3 +52,7 @@
     <?php } ?>
   </div>
 </div>
+<!-- <link rel="stylesheet" href="<?php echo site_url('assets/css/libs/dataTables.min.css'); ?>"> -->
+<script type="text/javascript">
+  $('.list-category-table').DataTable();
+</script>
