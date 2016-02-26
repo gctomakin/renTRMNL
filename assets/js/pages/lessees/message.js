d59979bf0fe2d528a09801791a67f912e8f2977d
Nichole Martinez <xin_martinez@yahoo.com>
2016-02-27 01:20:42 +0800

    Add lessee message script

---
+++ assets/js/pages/lessees/message.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+	var detailTemplate = _.template($('#message-detail-template').html());
+
+	$('#message-form').submit(function(e) {
+		e.preventDefault();
+		var body = $('#body-convo');
+		var text = $('#text-convo');
+		body.append(detailTemplate({name: 'Me', message: text.val(), 'position': 'pull-right'}));
+		body.animate({ scrollTop: body[0].scrollHeight - body.height() }, "slow");
+		text.val('');
+		text.focus();
+	});
+
+	$('#receiver').change(function() {
+		console.log($('#receiver :selected').text());
+		$('#name-convo').text($('#receiver :selected').text());
+		$('#btn-convo').prop('disabled', false);
+		$('#text-convo').prop('disabled', false);
+		$('#text-convo').text('');
+	});
+
+	$('#receiver').select2({
+		'placeholder': 'Choose a Lessor',
+		ajax: {
+			url: lessorListUrl,
+	    dataType: 'json',
+	    delay: 250,
+	    data: function (params) {
+	      return {
+	        q: params.term, // search term
+	        page: params.page
+	      };
+	    },
+	    processResults: function (data) {
+	    	console.log(data);
+	    	return { results: data };
+	    }
+		}
+	});
+}); 
