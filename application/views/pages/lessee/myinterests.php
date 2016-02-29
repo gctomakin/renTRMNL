9c856350426c4b9e972a37d5f49b9fad75cc0472
Nichole Martinez <xin_martinez@yahoo.com>
2016-02-29 23:54:22 +0800

    Fix lessee myinterest view

--- application/views/pages/lessee/myinterests.php
+++ application/views/pages/lessee/myinterests.php
@@ -12,43 +12,63 @@
         echo '<p class="alert alert-danger text-center"><strong>Failed!</strong> Unable to removed :(</p>';
     }
     ?>
-    <?php if(empty($myinterests)): ?>
+    <?php if(empty($items)): ?>
       <div class="col-md-12">
         <div class="alert alert-warning" role="alert"> <strong>Oops!</strong> Better check later, no items added yet. </div>
       </div>
     <?php else: ?>
-      <?php foreach($myinterests as $myinterest):?>
-        <div class="col-sm-3 col-lg-3 col-md-3">
-            <div class="thumbnail">
-              <?php
-                $itemPic = $myinterest->item_pic == NULL ? 
-                  'http://placehold.it/320x150' :
-                  'data:image/jpeg;base64,'.base64_encode($myinterest->item_pic);
-              ?>
-                <img src="<?php echo $itemPic; ?>" alt="">
-                <div class="caption">
-                    <h4><a href="#"><?php echo empty($myinterest->item_name) ? '<span class="text-danger">No Name</span>' : $myinterest->item_name; ?></a></h4>
-                    <p><?php echo $myinterest->item_desc; ?></p>
-                    <dl>
-                      <dt>Item Rate</dt>
-                      <dd><?php echo $myinterest->item_rate; ?></dd>
-                      <dt>Item Qty</dt>
-                      <dd><?php echo $myinterest->item_qty; ?></dd>
-                    </dl>
-                    <div class="btn-group">
-                      <a href="<?php echo site_url('reservations/item/' . $myinterest->item_id); ?>" class="btn btn-info btn-xs">Reserve</a>
-                      <a href="javascript:;" class="btn btn-success btn-xs btn-rent" data-item-id="<?php echo $myinterest->item_id; ?>">Rent</a>
-                      <a class="btn btn-danger btn-xs delete-trigger" data-item-name="<?php echo $myinterest->item_desc; ?>" data-id="<?php echo $myinterest->interest_id; ?>" href="<?php echo site_url("lessee/myinterests/delete/".$myinterest->interest_id); ?>" role="button"><span class="fa fa-trash"> Delete</span></a>
-                      <input type="hidden" value="<?php echo $myinterest->item_qty; ?>" class="item-qty">
-                      <input type="hidden" value="<?php echo $myinterest->item_rate; ?>" class="item-rate">
-                      <input type="hidden" value="<?php echo $myinterest->item_desc; ?>" class="item-desc">
-                      <input type="hidden" value="<?php echo $myinterest->item_rental_mode; ?>" class="item-mode">
-                      <input type="hidden" value="<?php echo $myinterest->subscriber_id; ?>" class="subscriber">
-                    
-                    </div>
-                </div>
-            </div>
+      <?php foreach($items as $item):
+        $itemLeft = $item['info']['item_qty'] - $item['info']['rented_qty'];
+      ?>
+      <div class="col-md-7 col-sm-7">
+        <a href="#">
+          <img src="<?php echo $item['info']['item_pic']; ?>" class="thumbnail img-responsive" alt="">
+        </a>
+      </div>
+      <div class="col-md-5 col-sm-5">
+        <h4>
+          <a href="#">
+            <?php echo empty($item['info']['item_name']) ? '<span class="text-danger">No Name</span>' : $item['info']['item_name']; ?>
+          </a>
+        </h4>
+        <p>(<?php echo $item['info']['item_desc']; ?>)</p>
+        <p>₱ <?php echo number_format($item['info']['item_rate'], 2); ?> 
+          <small>
+            <?php echo $rentalMode[$item['info']['item_rental_mode']]; ?>
+          </small>
+          <br>
+          <?php echo number_format($itemLeft); ?> pcs left
+        </p>
+        <p><a href="<?php echo site_url('lessee/shops?id=' . $item['info']['shop_id']); ?>"><?php echo $item['info']['shop']; ?></a></p>
+        <p>
+          <?php 
+            if (!empty($item['categories'])) {
+              $label = "<span class='label label-default'>";
+              echo $label;
+              $categoryType = array();
+              foreach ($item['categories'] as $category) {
+                $categoryType[] = $category->category_type;
+              }
+              echo implode("</span>  $label", $categoryType);
+              echo "</span>";
+            }
+          ?>
+        </p>
+        <div class="btn-group">
+          <a href="<?php echo site_url('reservations/item/' . $item['info']['item_id']); ?>" class="btn btn-info btn-xs">Reserve</a>
+          <?php if ($itemLeft > 0) { ?>
+          <button class="btn btn-success btn-xs btn-rent" data-item-id="<?php echo $item['info']['item_id']; ?>">Rent</button>
+          <?php } ?>
+          <a class="btn btn-danger btn-xs delete-trigger" data-item-name="<?php echo $item['info']['item_name']; ?>" data-id="<?php echo $item['myinterest']; ?>" href="<?php echo site_url("lessee/myinterests/delete/".$item['myinterest']); ?>" role="button"><span class="fa fa-trash"> Delete</span></a>
+          <input type="hidden" value="<?php echo $itemLeft; ?>" class="item-qty">
+          <input type="hidden" value="<?php echo $item['info']['item_rate']; ?>" class="item-rate">
+          <input type="hidden" value="<?php echo $item['info']['item_desc']; ?>" class="item-desc">
+          <input type="hidden" value="<?php echo $item['info']['item_name']; ?>" class="item-name">
+          <input type="hidden" value="<?php echo $item['info']['item_rental_mode']; ?>" class="item-mode">
+          <input type="hidden" value="<?php echo $item['info']['subscriber_id']; ?>" class="subscriber">
         </div>
+      </div>
+      <div class="col-md-12"><hr></div>
       <?php endforeach; ?>
       <!--pagination-->
       <div class="row text-center">
@@ -81,7 +101,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <a role="button" class="btn btn-danger" href="<?php echo site_url("lessee/myinterests/delete/".$myinterest->interest_id); ?>">Remove</a>
+                <a role="button" class="btn btn-danger" id="remove-myinterest" href="">Remove</a>
             </div>
         </div>
         <!-- /.modal-content -->
@@ -118,6 +138,16 @@
   </div>
   <!-- /.modal-dialog -->
 </div>
+<form id='paypal-form' class="standard" action="https://www.sandbox.paypal.com/webapps/adaptivepayment/flow/pay" target="PPDGFrame">
+  <input id="type" type="hidden" name="expType" value="light">
+  <input id="paykey" type="hidden" name="paykey" value="">
+  <input class="hidden" type="submit" id="btn-pay"> 
+</form>
+<script src="https://www.paypalobjects.com/js/external/dg.js" type="text/javascript"></script>
 <script>
+  var dgFlow = new PAYPAL.apps.DGFlow({ trigger: 'btn-pay'});
   var rentUrl = "<?php echo site_url('reservations/save'); ?>";
+  var checkItemRentedUrl = "<?php echo site_url('rental/checkItemRented'); ?>";
+  var rentItemUrl = "<?php echo site_url('rental/item'); ?>";
+  var reservationUrl = "<?php echo site_url('reservations/save'); ?>";
 </script>
