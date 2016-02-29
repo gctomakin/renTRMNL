38757f758c42cdcef80114580981733e4bf0ec2f
Nichole Martinez <xin_martinez@yahoo.com>
2016-02-29 18:39:49 +0800

    Remove notification panel

--- application/views/templates/lessee/sidebar.php
+++ application/views/templates/lessee/sidebar.php
@@ -60,20 +60,17 @@
             </ul>
             &nbsp;
             <!-- notification -->
-            <div class="panel panel-default">
+            <!--<div class="panel panel-default">
                 <div class="panel-heading">
                     <i class="fa fa-bell fa-fw"></i> Notifications Panel
                 </div>
-                <!-- /.panel-heading -->
                 <div class="panel-body">
                     <div class="list-group" id="notify-list">
 
                     </div>
-                    <!-- /.list-group -->
                     <a href="#" class="btn btn-default btn-block">View All Alerts</a>
                 </div>
-                <!-- /.panel-body -->
-            </div>
+            </div> -->
             <!-- notification end -->
         </div>
         <!-- /.sidebar-collapse -->
--- application/views/templates/lessor/sidebar.php
+++ application/views/templates/lessor/sidebar.php
@@ -63,20 +63,18 @@
                     <!-- /.nav-second-level -->
                     &nbsp;
                     <!-- notification -->
-                    <div class="panel panel-default">
+                    <!--<div class="panel panel-default">
                         <div class="panel-heading">
                             <i class="fa fa-bell fa-fw"></i> Notifications Panel
                         </div>
-                        <!-- /.panel-heading -->
                         <div class="panel-body">
                             <div class="list-group" id="notify-list">
 
                             </div>
-                            <!-- /.list-group -->
                             <a href="#" class="btn btn-default btn-block">View All Alerts</a>
                         </div>
-                        <!-- /.panel-body -->
                     </div>
+                    -->
                     <!-- notification end -->
                 </li>
             </ul>
