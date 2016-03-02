9d179252dc279eebd7bda40814a383f408be2ff4
Nichole Martinez <xin_martinez@yahoo.com>
2016-03-03 03:00:51 +0800

    Fix lessee items rented view

--- application/views/pages/lessee/items/rented.php
+++ application/views/pages/lessee/items/rented.php
@@ -4,28 +4,36 @@
   		<table class="table">
   			<thead>
   				<tr>
+            <th>RESERVATION # </th>
   					<th>DESCRIPTION</th>
   					<th>AMOUNT</th>
   					<th>QUANTITY</th>
   					<th>DURATION</th>
   					<th>SHOP</th>
-  					<th>OPTION</th>
+  					<th></th>
   				</tr>
   			</thead>
   			<tbody>
           <?php if (empty($items)) { ?>
-          <tr><td colspan="6"><h2 class="text-danger">No Item Rented as of now.</h2></td></tr>
+          <tr><td colspan="7"><h2 class="text-danger">No Item Rented as of now.</h2></td></tr>
           <?php } else { ?>
     				<?php foreach ($items as $item) { ?>
     				<tr>
+              <td>
+                <a href="<?php echo site_url('lessee/reserved?id=' . $item['reserve_id']); ?>">
+                  <?php echo $item['reserve_id']; ?>
+                </a>
+              </td>
     					<td><a href="<?php echo site_url('lessee/items/?item=' . $item['item_desc']); ?>" target="_blank"><?php echo $item['item_desc']; ?></a></td>
     					<td><?php echo $item['rental_amt']; ?></td>
     					<td><?php echo $item['qty']; ?></td>
     					<td><?php echo $item['duration']; ?></td>
     					<td><?php echo $item['shop']; ?></td>
     					<td>
+                <?php if ($item['total_balance'] == 0) { ?>
     						<button class="btn btn-success btn-xs btn-return" data-rev-id="<?php echo $item['reserve_id']; ?>">return</button>
-    						<button class="btn btn-default btn-xs btn-reservation" data-rev-id="<?php echo $item['reserve_id']; ?>">view transaction</button>
+                <?php } ?>
+    						<!-- <button class="btn btn-default btn-xs btn-reservation" data-rev-id="<?php echo $item['reserve_id']; ?>">view transaction</button> -->
     					</td>
     				</tr>
     				<?php } ?>
@@ -52,6 +60,5 @@
   <!-- /.modal-dialog -->
 </div>
 <script>
-  var reservationUrl = "<?php echo site_url('reservations/view'); ?>";
   var returnUrl = "<?php echo site_url('reservations/returnStatus'); ?>"
 </script>
