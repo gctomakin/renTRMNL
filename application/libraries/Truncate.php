10506f4048a99ee27b7374a0ed5c8117e9e1e990
Nichole Martinez <xin_martinez@yahoo.com>
2016-03-04 23:26:54 +0800

    Add Truncate lib

---
+++ application/libraries/Truncate.php
@@ -0,0 +1,17 @@
+<?php
+class Truncate {
+
+	function trap($string, $limit, $break=".", $pad="...") {
+	  // return with no change if string is shorter than $limit
+	  if(strlen($string) <= $limit) return $string;
+
+	  // is $break present between $limit and the end of the string?
+	  if(false !== ($breakpoint = strpos($string, $break, $limit))) {
+	    if($breakpoint < strlen($string) - 1) {
+	      $string = substr($string, 0, $breakpoint) . $pad;
+	    }
+	  }
+
+	  return $string;
+	}
+}
